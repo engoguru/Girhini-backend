3dbf8386e2ef847d59523be6b37eb2c59015506d
engoguru <engoguru@carkverma.com>
2025-10-27 10:52:28 +0530

    cors error

--- server.js
+++ server.js
@@ -39,10 +39,17 @@ app.use(helmet());
 app.use(cookieParser())
 // CORS
 const allowedOrigins = ['http://localhost:5173'];
-app.use(cors({
-  origin: allowedOrigins,
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps, curl, etc.)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) return callback(null, true);
+      return callback(new Error("Not allowed by CORS"));
+    },
+    credentials: true, // allow cookies, auth headers
+  })
+);
 app.use(express.json());
 // DB
 
